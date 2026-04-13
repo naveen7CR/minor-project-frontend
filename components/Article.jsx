20bcbf5b0d01b80d50fb8ee9054ece89e4e1acac
Naveen Singh <naveenshubham620@gmail.com>
2026-04-13 12:23:59 +0530

    day 2 commit

--- components/Article.jsx
+++ components/Article.jsx
@@ -1,2 +1,33 @@
 import React from 'react'
 
+function Article(props) {
+    const [count, setCount] = React.useState(0)
+
+    const handleIncrement = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <h1>Article</h1>
+            <h3>{props.title}</h3>
+            <p>lorem ipsum dolor sit amet, consectetur adipiscing elit  </p>
+            <h2>{count}</h2>
+            <button onClick={handleIncrement}>Like ({count})</button>
+        </div>
+    )
+}
+
+export default Article
+
+
+
+import styled from 'styled-components';
+const Button = styled.button`
+    background-color: #4CAF50; 
+    border: none;
+    color: white;
+`;
+
+<Button>Click Me</Button>
+
